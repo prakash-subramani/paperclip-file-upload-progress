702d916f6b87195bacee9c48e4d83442e16b68a7
Madhan Kumar S <35567949+madhankumardroid@users.noreply.github.com>
2018-10-27 10:56:29 +0530

    Alignment changes
    Alignment changes

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,9 +1,7 @@
 class Content < ApplicationRecord
-
   has_attached_file :attachment,
                     :url => "/attachments/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/attachments/:id/:style/:basename.:extension"
 
   validates_attachment_content_type :attachment, :content_type => [/.*/]
-
 end
